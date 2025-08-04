183487b334ec1f8affc15ba7cd89c1dbadf1b7f4
ARP <ABDULREHMANSECOURSE@GMAIL.COM>
2025-08-04 10:01:00 +0500

    hj

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ alert (fullname)
 var a = '5';
 var b = 2;
 var result = a + b;
-console.log(result);
+console.log("2 ; " + result);
 
 var a = 10;
 var b = 5;
 var c = ++a + b-- + --b;
-console.log(c);
+console.log("3 ;" + c);
 var explanation = "(++a = 11) (b-- = 5) (--b (b pehle 4 ho chuka tha) = 3) (Total: 11 + 5 + 3 = 19)"
 console.log(explanation)
 
 var beeees = '20'
-alert ("4 ; "+beeees *3)
+alert ("4 ; " +beeees *3)
 
 var percentage = 15 % 7;
-console.log(percentage);
+console.log("5 ; " + percentage);
 
 var student = "Hassan";
 var score = 88;
@@ -27,7 +27,7 @@ alert("6 ; "+student + " got " + score + " marks in JavaScript!");
 
 let x = 5;
 let y = x++ + ++x - --x;
-console.log(y);
+console.log("7 ; " + y);
 
 let perfumePrice = 2500;
 let total = perfumePrice * 4;
@@ -38,7 +38,7 @@ var co2 = 7
 var co3 = 8
 var co4 = 9
 var co = co1 * co2 / co3 + co4
-console.log(co)
+console.log("10 ; " + co)
 
 var msg = "Total: " + (5 + 5) * 2;
 alert("11 ; "+msg); 
