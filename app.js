5fbe96ce8ed206b35cf647327dc9ea82e45b10f1
ARP <ABDULREHMANSECOURSE@GMAIL.COM>
2025-08-04 09:52:14 +0500

    a

---
+++ app.js
@@ -0,0 +1,44 @@
+var fname = "1 ; Abdul ";
+var lname = "Rehman";
+var fullname = fname+lname
+alert (fullname)
+
+var a = '5';
+var b = 2;
+var result = a + b;
+console.log(result);
+
+var a = 10;
+var b = 5;
+var c = ++a + b-- + --b;
+console.log(c);
+var explanation = "(++a = 11) (b-- = 5) (--b (b pehle 4 ho chuka tha) = 3) (Total: 11 + 5 + 3 = 19)"
+console.log(explanation)
+
+var beeees = '20'
+alert ("4 ; "+beeees *3)
+
+var percentage = 15 % 7;
+console.log(percentage);
+
+var student = "Hassan";
+var score = 88;
+alert("6 ; "+student + " got " + score + " marks in JavaScript!");
+
+let x = 5;
+let y = x++ + ++x - --x;
+console.log(y);
+
+let perfumePrice = 2500;
+let total = perfumePrice * 4;
+alert("9 ; "+"Total bill: " + total + " PKR");
+
+var co1 = 6
+var co2 = 7
+var co3 = 8
+var co4 = 9
+var co = co1 * co2 / co3 + co4
+console.log(co)
+
+var msg = "Total: " + (5 + 5) * 2;
+alert("11 ; "+msg); 
